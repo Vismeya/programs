cd32b35775e54820181774f846c37abc0ae8aed0
Vismeya <raghavi3197@gmail.com>
2017-06-07 17:49:16 +0530

    Update pal.java

--- pal.java
+++ pal.java
@@ -5,26 +5,24 @@ public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
-int s=0,sum=0,v,k;
+int s=0,sum=0,v,k,temp;
 while(n!=0)
 {
 k=n%10;
 sum=sum+k;
 n=n/10;
 }
+System.out.println("sum is:" +sum);
+temp=sum;
 while(sum>0)
 {
 v=sum%10;
 s=(s*10)+v;
 sum=sum/10;
 }
-if(n==s)
-{
+if(temp==s)
 System.out.println("palindrome");
-}
 else
-{
 System.out.println("not a palindrome");
-}
 }
 }
