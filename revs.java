d62b00d5fbecd2d87c6470ec9b18a21053f099ac
Vismeya <raghavi3197@gmail.com>
2017-06-07 17:00:25 +0530

    Create revs.java

---
+++ revs.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class revs
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int rev=0,v;
+while(a!=0)
+{
+v=a%10;
+rev=(rev*10)+v;
+a=a/10;
+}
+System.out.println(rev);
+}
+}
+
+
