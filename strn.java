d5d0c669cf7b6b6631ed65cfdf1237b280a2e093
Vismeya <raghavi3197@gmail.com>
2017-06-07 14:27:31 +0530

    Create strn.java

---
+++ strn.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class strn
+
+{
+   
+ public static void main(String args[])
+  
+  {
+    
+  StringBuffer sb=new StringBuffer(args[0]);
+     
+ System.out.println(sb.reverse());
+   
+ }
+
+}
